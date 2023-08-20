b3c2177ea090c8ab1e0a06c9e9906d81e66fb193
WolfGenerals <2301146268@qq.com>
2023-08-20 11:01:12 +0800

    适配1.19.2

--- src/main/java/com/github/wolfgenerals/petphraseplus/mixin/ChatScreenMixin.java
+++ src/main/java/com/github/wolfgenerals/petphraseplus/mixin/ChatScreenMixin.java
@@ -4,7 +4,10 @@ package com.github.wolfgenerals.petphraseplus.mixin;
 import com.github.wolfgenerals.petphraseplus.ModifierKt;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ChatScreen;
+import net.minecraft.client.network.ChatPreviewer;
+import net.minecraft.util.Util;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,15 +19,16 @@ public abstract class ChatScreenMixin {
 
     @Unique
     protected final MinecraftClient client = MinecraftClient.getInstance();
+    @Shadow
+    private ChatPreviewer chatPreviewer;
 
 
     @Inject(method = "sendMessage(Ljava/lang/String;Z)Z", at = @At("HEAD"), cancellable = true)
     public void sendMessage(String chatText, boolean addToHistory, CallbackInfoReturnable<Boolean> cir) {
-        if (ModifierKt.needModify(chatText)) {
-            if (addToHistory) this.client.inGameHud.getChatHud().addToMessageHistory(chatText);
-            this.client.player.networkHandler.sendChatMessage(ModifierKt.modifyMessage(chatText));
-            cir.setReturnValue(true);
-        }
+        if (!ModifierKt.needModify(chatText)) return;
+        if (addToHistory) this.client.inGameHud.getChatHud().addToMessageHistory(chatText);
+        this.client.player.sendChatMessage(ModifierKt.modifyMessage(chatText), Util.map(this.chatPreviewer.tryConsumeResponse(chatText), ChatPreviewer.Response::previewText));
+        cir.setReturnValue(true);
     }
 }
 
